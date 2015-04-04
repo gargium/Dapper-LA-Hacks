8386e632cc9f3252237be0e7bd17961b2ab088a5
Rakshit Garg <raks.garg@gmail.com>
2015-04-04 00:06:25 -0700

    Initial Commit

---
+++ Dappr/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Dappr
+//
+//  Created by Gargium on 4/3/15.
+//  Copyright (c) 2015 rakshitgarg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "init.png")!);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
