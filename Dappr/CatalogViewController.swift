7e8df8cc003054d24858fd156eca467341d72d42
Shivam Thapar <shivam.thapar1@gmail.com>
2015-04-04 13:40:43 -0700

    catalog view, basic json traversing

--- Dappr/CatalogViewController.swift
+++ Dappr/CatalogViewController.swift
@@ -9,17 +9,57 @@
 import Foundation
 import UIKit
 
-class CatalogViewController : UIViewController{
+class CatalogViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    @IBOutlet var collectionView: UICollectionView?
+    var json: JSON?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        printJSON("mens_collection")
+        // Do any additional setup after loading the view, typically from a nib.
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 70, left: 5, bottom: 10, right: 5)
+        layout.itemSize = CGSize(width: 202, height: 270)
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.registerClass(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+        fetchJSON("mens_collection")
+        if json != nil{
+            printJSON()
+        }
+        self.view.addSubview(collectionView!)
     }
     
-    func printJSON(jsonFileName : String) {
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // number of elems
+        return 3
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as CollectionViewCell
+        cell.backgroundColor = UIColor.blackColor()
+        cell.textLabel?.text = "\(indexPath.section):\(indexPath.row)"
+        cell.imageView?.image = UIImage(named: "circle")
+        return cell
+    }
+    
+    func fetchJSON(jsonFileName : String){
         if let path = NSBundle.mainBundle().pathForResource(jsonFileName, ofType: "json") {
             if let data = NSData(contentsOfMappedFile: path) {
-                let json = JSON(data: data, options: NSJSONReadingOptions.AllowFragments, error: nil)
-                println("jsonData:\(json)")
+                self.json = JSON(data: data, options: NSJSONReadingOptions.AllowFragments, error: nil)
+            }
+        }
+    }
+    
+    func printJSON() {
+        if let items = self.json!["mens"]["OldNavy"]["khakis"].array{
+            for item in items{
+                //println(item["price"].string)
             }
         }
     }
